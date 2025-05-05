94613221d633e8704257751991e060fc6dce50d8
Omar Faruk <omarfaruk.dev@gmail.com>
2025-05-05 17:34:00 +0600

    my bill page conditional bill type icon added

--- src/pages/Bills/MyBills.jsx
+++ src/pages/Bills/MyBills.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { FaBolt, FaRegCreditCard, FaUniversity, FaWifi } from 'react-icons/fa';
+import { GiGasStove } from 'react-icons/gi';
+import { IoWater } from 'react-icons/io5';
+import { MdLiveTv } from 'react-icons/md';
 import { Link, useLoaderData } from 'react-router';
 
 const formatDate = (isoDate) => {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     return new Date(isoDate).toLocaleDateString(undefined, options);
+};
+
+
+const getBillIcon = (type) => {
+    const lowerType = type.toLowerCase();
+
+    if (lowerType === 'electricity') {
+        return <FaBolt className="text-yellow-500 text-xl" />;
+    } else if (lowerType === 'water') {
+        return <IoWater className="text-blue-500 text-xl" />;
+    } else if (lowerType === 'gas') {
+        return <GiGasStove className="text-red-500 text-xl" />;
+    } else if (lowerType === 'tuition') {
+        return <FaUniversity className="text-green-600 text-xl" />;
+    } else if (lowerType === 'internet') {
+        return <FaWifi className="text-indigo-500 text-xl" />;
+    } else if (lowerType === 'creditcard') {
+        return <FaRegCreditCard  className="text-green-500 text-xl"/>;
+    } else if (lowerType === 'satellitetv') {
+        return <MdLiveTv  className="text-blue-400 text-xl"/>;
+    }
+    
+    else {
+        return null;
+    }
 };
 
 const MyBills = () => {
@@ -21,8 +50,11 @@ const MyBills = () => {
                             className="flex flex-col md:flex-row items-center justify-between space-y-2 p-4 rounded-xl shadow-md border border-blue-700"
                         >
                             <div className="flex flex-col md:flex-row items-center gap-4">
-                                <div className="bg-blue-100 p-2 rounded-lg">
-                                    <img src={bill.icon} alt={bill.bill_type} className="w-30 h-30 object-contain rounded-lg shadow" />
+                                <div className="relative bg-blue-100 p-1 rounded-lg">
+                                    <img src={bill.icon} alt={bill.bill_type} className="w-40 h-40 object-contain rounded-lg shadow" />
+                                    <div className="absolute bottom-3 right-3 bg-white p-1 rounded-lg shadow-md">
+                                        {getBillIcon(bill.bill_type)}
+                                    </div>
                                 </div>
                                 <div>
                                     <h3 className="font-semibold text-xl text-blue-900">{bill.organization}</h3>
